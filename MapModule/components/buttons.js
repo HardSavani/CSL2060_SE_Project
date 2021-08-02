7897a77ecd4a53af5d0de4f32e6826899653b469
Shivam1007 <55546051+Shivam1007@users.noreply.github.com>
2021-08-03 00:33:45 +0530

    Updating Comments

--- MapModule/components/buttons.js
+++ MapModule/components/buttons.js
@@ -17,8 +17,7 @@ const FooterButtonContainer = props => {
         backgroundColor: "skyblue"
       }}
     >
-      {/* <View style={{ flex: 1 }}> */}
-      {/* <Button title=" Contact Us" /> */}
+
       <TouchableHighlight
         style={{ flex: 1, paddingVertical: 8 }}
         underlayColor="#DDDDDD"
@@ -35,10 +34,6 @@ const FooterButtonContainer = props => {
           <Text>Contact Us</Text>
         </View>
       </TouchableHighlight>
-      {/* </View> */}
-      {/* <View style={{ flex: 1 }}>
-        <Button title="Help" />
-      </View> */}
 
       <TouchableHighlight
         style={{ flex: 1, paddingVertical: 8 }}
@@ -62,11 +57,9 @@ const FooterButtonContainer = props => {
 
 const styles = StyleSheet.create({
   buttonContainer: {
-    // flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
     height: 45
-    // alignItems: "space-between"
   }
 });
 
