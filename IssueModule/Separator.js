21717349a3976fe1ec23eb3884f0bf3e01f9899d
HardSavani <58904576+HardSavani@users.noreply.github.com>
2021-08-03 13:11:22 +0530

    Removed unnecessary comments

--- IssueModule/Separator.js
+++ IssueModule/Separator.js
@@ -4,14 +4,12 @@ import { StyleSheet, View } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
-    // backgroundColor: "yellow",
     justifyContent: "center",
     padding: 10,
   },
   separator: {
     borderColor: "grey",
     borderWidth: 1,
-    // flex: 1,
     width: "90%",
     flexDirection: "row",
   },
